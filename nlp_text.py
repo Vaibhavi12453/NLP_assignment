eba62533e665e74956f818ac4cf2f78d31ea1664
Vaibhavi12453 <vaibhavidevare36@gmail.com>
2025-09-09 16:08:25 +0530

    Update nlp_text.py

--- nlp_text.py
+++ nlp_text.py
@@ -1,12 +1,12 @@
 import nltk
 
 text = """
-Name: Sanskruti Ravi Deshmukh.
-Roll No: 33
+Name: vaibhavi devare
+Roll No: 35
 Year: B.Tech
 Branch: Information Technology.
 Mobile no: 0987654321
-Email: sanku@gmail.com
+Email: vaibhavi@gmail.com
 """
 
 tokens = nltk.word_tokenize(text)
@@ -14,4 +14,5 @@ print(tokens)
 
 
 #(venv) PS C:\Users\Shree\Desktop\NLP> python Assigenment1.py
+
 # ['Name', ':', 'Gawali', 'Tejshree', 'Tulshidas', '.', 'Roll', 'No', ':', '47', 'Year', ':', 'B.Tech', 'Branch', ':', 'Information', 'Technology', '.', 'Mobile', 'no', ':', '0872392648716', 'Email', ':', 'tej', '@', 'gmail.com']
