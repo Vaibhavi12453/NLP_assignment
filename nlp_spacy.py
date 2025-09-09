fff72dd4afd9d1c0d3b52e06485a3d2d903b5e10
Vaibhavi12453 <vaibhavidevare36@gmail.com>
2025-09-09 16:07:42 +0530

    Update nlp_spacy.py

--- nlp_spacy.py
+++ nlp_spacy.py
@@ -4,7 +4,7 @@ import string
 nlp = spacy.load("en_core_web_sm")
 
 text = """
-My name is Sanskruti Deshmukh, a final-year Information Technology Engineering student from Kopargaon.
+My name is vaibhavi devare, a final-year Information Technology Engineering student from Kopargaon.
 I study at Sanjivani College of Engineering and have a deep interest in AIMl, IoT, and cybersecurity.
 I have worked on several projects including a Smart Health Monitoring System and an Inventory Management System using the MERN stack.
 
@@ -18,5 +18,6 @@ lemmatized = [token.lemma_ for token in doc if not token.is_stop and token.text 
 
 print("Filtered Tokens:", filtered_tokens)
 print("==========================================================")
+
 
 print("Lemmatized Tokens:", lemmatized)
